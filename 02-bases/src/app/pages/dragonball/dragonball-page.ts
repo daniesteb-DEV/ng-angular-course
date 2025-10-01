69fae4e122d8bcdaaf238cd9a55b746ce5ccb2ff
DANIEL ESTEBAN JIMENEZ ANGAMARCA <danielejimeneza@gmail.com>
2025-10-01 09:25:12 -0500

    Enhances character form and listing logic
    Introduces reactive state management for character name and power fields, enabling dynamic input and addition of new characters.
    
    Improves character listing by displaying indexed entries, conditional styling based on power level, and filtering out low-powered characters for clearer presentation.
    
    Adds CLI analytics configuration for project insights.

--- 02-bases/src/app/pages/dragonball/dragonball-page.ts
+++ 02-bases/src/app/pages/dragonball/dragonball-page.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { Navbar } from '../../components/shared/navbar/navbar';
+import { NgClass } from '@angular/common';
 
 interface Character {
   id: number;
@@ -9,14 +10,35 @@ interface Character {
 
 @Component({
   selector: 'app-dragonball-page',
-  imports: [Navbar],
+  imports: [Navbar, NgClass],
   templateUrl: './dragonball-page.html',
 })
 export class DragonballPage {
+  name = signal('Bulma');
+  power = signal(100);
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 9001 },
     { id: 2, name: 'Vegeta', power: 8500 },
     { id: 3, name: 'Piccolo', power: 3000 },
     { id: 4, name: 'Gohan', power: 8000 },
+    { id: 4, name: 'Yamcha', power: 500 },
   ]);
+
+  powerClasses = computed(() => {
+    return {
+      'text-danger': this.characters().some(character => character.power > 9000),
+      'text-primary': this.characters().some(character => character.power < 9000),
+    }
+  });
+
+  addCharacter() {
+    console.log({ name: this.name(), power: this.power() });
+    const newCharacter:Character = {
+      id: this.characters().length + 1,
+      name: this.name(),
+      power: this.power(),
+    };
+    // this.characters().push(newCharacter);
+    this.characters.update(characters => [...characters, newCharacter]);
+  }
 }
